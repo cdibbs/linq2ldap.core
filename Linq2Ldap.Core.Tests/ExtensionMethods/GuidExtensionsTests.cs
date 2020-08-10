ea31c682e1258a83dcdf8c38a7cba9035de914a1
Sergey Tikhonov <sergey.tikhonov@helios-gesundheit.de>
2020-08-10 16:02:41 +0200

    Unit test correction for better readability

--- Linq2Ldap.Core.Tests/ExtensionMethods/GuidExtensionsTests.cs
+++ Linq2Ldap.Core.Tests/ExtensionMethods/GuidExtensionsTests.cs
@@ -17,9 +17,9 @@ namespace Linq2Ldap.Core.Tests.ExtensionMethods
         [Fact]
         public void ToEscapedBytesString_BytesArray()
         {
-            var bytes = new Guid("96AA667A-E58E-486D-9114-CB1EDC5E2B0D").ToByteArray();
+            var bytes = new byte[] {0xAB, 0x12, 0xFF, 0x00};
             var result = bytes.ToEscapedBytesString();
-            Assert.Equal(@"\7a\66\aa\96\8e\e5\6d\48\91\14\cb\1e\dc\5e\2b\0d", result);
+            Assert.Equal(@"\ab\12\ff\00", result);
         }
     }
 }
